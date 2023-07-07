4d3474511a5f22e45736bda6817734ed190358e6
Tanner Bitz <tannerbitz@gmail.com>
2023-07-06 23:34:44 -0400

    adds tmux config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -4,7 +4,9 @@ dotfiles_dir=~/repos/dotfiles
 
 sudo rm -rf ~/.config/i3
 sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.tmux.conf
 
 ln -sf $dotfiles_dir/xdg_config/i3 ~/.config/i3
 ln -sf $dotfiles_dir/xdg_config/nvim ~/.config/nvim
+ln -sf $dotfiles_dir/tmux/.tmux.conf ~/.tmux.conf
 
