3043543d345c2ac04869f0cbf238b186173778a9
Tanner Bitz <tannerbitz@gmail.com>
2023-07-06 23:30:20 -0400

    create symlinks script

---
+++ create-symlinks.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+dotfiles_dir=~/repos/dotfiles
+
+sudo rm -rf ~/.config/i3
+sudo rm -rf ~/.config/nvim
+
+ln -sf $dotfiles_dir/xdg_config/i3 ~/.config/i3
+ln -sf $dotfiles_dir/xdg_config/nvim ~/.config/nvim
+
