581a4fca6964ea1318556cb6872991548f36540d
Tanner Bitz <tbitz@asensus.com>
2024-01-19 12:59:07 -0500

    adds remap to telescope 'lsp_references'

--- xdg_config/nvim/after/plugin/telescope.lua
+++ xdg_config/nvim/after/plugin/telescope.lua
@@ -57,3 +57,5 @@ vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch curren
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>fg', lga_shortcuts.grep_visual_selection, { desc = 'Live Grep Args' })
+
+vim.keymap.set('n', '<leader>re', builtin.lsp_references, { desc = "LSP References" })
