e45a9d2480e091d9fb54b713de66b7b416224271
Tanner Bitz <tannerbitz@gmail.com>
2026-05-08 16:38:06 -0400

    execute lua files

--- xdg_config/nvim/lua/custom/tbitz/remap.lua
+++ xdg_config/nvim/lua/custom/tbitz/remap.lua
@@ -27,6 +27,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>rl', ":luafile %<CR>", { desc = "[R]un [L]uafile" })
 
 -- center cursor when doing half page jumps
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
